a75ca3eedcff46ab9d71b049a9cfddc1b2be0672
IanEdwards99 <EDWIAN004@myuct.ac.za>
2021-06-12 11:20:10 +0200

    Created Classifier.py

---
+++ Classifier.py
@@ -0,0 +1,20 @@
+#Author: Ian Edwards EDWIAN004
+#CSC3022F ML assignment 3
+
+import random
+from random import seed
+from random import randint
+import sys
+import argparse
+#text for argument parser:
+text = 'The XOR gate program implements a XOR gate with a feed forward Perceptron ANN' 
+
+def main(): #get arguments passed in.
+    print("Pytorch Output...")
+
+    print("Done!")
+
+    path = input("Please enter a filepath:")
+    while (path != "exit"):
+        
+    
